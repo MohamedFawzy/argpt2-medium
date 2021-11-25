c8bdb7ce29e01a548298ca17b0619cdfad7f2bf6
MohamedFawzy <mfawzy22@gmail.com>
2021-11-26 00:54:10 +0400

    add all documents to a single file

--- preprocessing.py
+++ preprocessing.py
@@ -1,5 +1,14 @@
 from pathlib import Path
 import os
+import tqdm
 
 # the folder 'text' contains all the files
 paths = [str(x) for x in Path("./ar_corpus/").glob("**/*.txt")]
+single_string = ""
+for filename in tqdm.tqdm(paths):
+    with open(filename, "r", encoding="utf-8") as f:
+        x = f.read()
+    single_string += x
+with open("data.txt", "w") as file_handle:
+    file_handle.write(single_string)
+
